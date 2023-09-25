05eafda5c670524f64a45ce174c57b5180448b7d
Abuxworkz <abu.xworkz@gmail.com>
2023-09-25 18:02:48 +0530

    task1

---
+++ Array1.java
@@ -0,0 +1,10 @@
+public class Array1 {
+	public static void main(String[] args){
+		short studentid[] = {1001,2020,3333,4567,2323,6457,1234,4523,7563,1298};
+		System.out.println(studentid[4]);
+
+		for(int j=0;j < studentid.length;j++){
+			System.out.println(studentid[j]);
+		}
+	}
+  }
