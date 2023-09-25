6af207dab8eea55e0a15bce063392c58c0697bcb
Abuxworkz <abu.xworkz@gmail.com>
2023-09-25 17:42:34 +0530

    task 1

---
+++ Array.java
@@ -0,0 +1,10 @@
+public class Array{
+   public static void main(String args[]){
+    byte[] marks={45,67,34,72,10,63,37,28,11,49};
+    System.out.println(marks[5]);
+
+     for(int i=0;i<10;i++){
+         System.out.println(marks[i]);
+     }
+    }
+  }
